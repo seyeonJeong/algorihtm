79d5525b9b6132f208597427e983f27774cb0ccb
Seyeon <wjdtpdus828@naver.com>
2023-02-16 15:14:50 +0900

    23/02/16

--- Queue/boj_2164.py
+++ Queue/boj_2164.py
@@ -1,25 +1,22 @@
+from collections import deque
 
+q = deque()
 n = int(input())
 
-queue = [0 for i in range(n)]
-rear = 0
-front = 0
+
 
 for i in range(n):
-    queue[i] = i+1
-    rear += 1
+    q.append(i+1)
 
 
-while rear != (front+1):
+while len(q) != 1:
     tmp = 0
-    queue[front] = 0
-    front += 1
-    cur = front
-    while (cur+1) != rear:
-        queue[cur],queue[cur+1] = queue[cur+1],queue[cur]
-        cur += 1
+    q.popleft()
+    a = q.popleft()
+    q.append(a)
 
 
-print(queue[-1])
 
-# 계속 틀렸다해서 결과를 보니 deque를 쓰라고 한다.. deque 공부하고 다시 풀어야함
+print(q[0])
+
+# deque로 풀었음 2023/2/16
